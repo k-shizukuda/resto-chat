cc78973bc589a1e90ead611033cf743c9b1a2f6b
k-shizukuda <k.shizukuda@icloud.com>
2020-08-15 23:26:31 +0900

    fix-message-bag

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,7 +1,6 @@
   $(function(){
 
   $('.Checkbox__box').on('click',function(){
-    console.log("aa")
     if($(this).prop('checked')){
       $('.TableFormBtn').removeClass('HiddenBtn')
     }else{
